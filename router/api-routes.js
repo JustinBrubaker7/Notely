d3999d8eb4e37d9c0b03f5f138f02c426695d91b
Justin Brubaker <justinbrubaker7@gmail.com>
2021-04-16 10:06:59 -0700

    started post routes

--- router/api-routes.js
+++ router/api-routes.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../db/db.json')
+const fs = require('fs');
 
 router.get('/notes', (req,res)=> {
     res.header("Content-Type",'application/json');
     res.send(JSON.stringify(data));
 })
 
+router.post('/notes', (req,res)=> {
 
+    const newNotes = req.body;
 
+    fs.appendFile('db.json', newNotes, (err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+      });
+})
 
 
 
